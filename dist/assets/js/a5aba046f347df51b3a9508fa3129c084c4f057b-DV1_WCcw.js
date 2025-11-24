655f3de8d8eecc307ec19889f88c38a9ee62925a
sondrdesigns <studio@sondrdesigns.com>
2025-11-24 13:30:11 -1000

    Revert to public folder image paths - use /images/ paths that worked before

--- dist/assets/js/a5aba046f347df51b3a9508fa3129c084c4f057b-DV1_WCcw.js
+++
@@ -1,1 +0,0 @@
-const a="/assets/png/a5aba046f347df51b3a9508fa3129c084c4f057b-CQ2nCYc7.png";export{a as b};
--- dist/assets/js/mail-BlFHwyva.js
+++ dist/assets/js/mail-BlFHwyva.js
@@ -1,4 +1,4 @@
-import{c}from"./index-Cr8Das6J.js";/**
+import{c}from"./index-hmnk-GPx.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
